1db3e392bdca42b343b08a450dc420904310ba70
Guilherme Silva Guini <guilherme.guini@matera.com>
2020-06-06 14:24:12 -0300

    Adding dockerfile and main class

---
+++ src/main/java/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    
+    public static void main(String[] args) {
+        System.out.println("This project have examples of structural and creational patterns (GOF).");
+    }
+}
